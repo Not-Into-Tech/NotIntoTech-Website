a9dfc3c4ce9a84c3f6b6b7b08bf680599f249968
Muhammad Adnan Bayu Firdaus <adnanbayu.firdaus@gmail.com>
2026-05-05 15:57:30 +0700

    fixed ejs miss configuration (2)

--- server-local.js
+++ server-local.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'src/public')));
 
 app.set('view engine', 'ejs');
-app.set('views');
+app.set('views', path.join(__dirname, 'views'));
 
 // Proxy Route for Chatbot
 app.post('/api/chat', async (req, res) => {
